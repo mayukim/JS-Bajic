53a7b00f11c593245fc235b9bf34fba6fd94a1b0
mayukim <111472634+mayukim@users.noreply.github.com>
2023-12-28 23:59:56 +0900

    Study 2.0~2.5-김마유

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-alert("hi");
+// alert("hi");
+
+// console.log(54545454);
+
+// console.log("lalalalalala");
+
+// console.log("lalalalalala");
+
+// const a = 5;
+// const b = 2;
+// const veryLongVariableName = 0;
+
+// const myName = "mayu";
+
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log("hello " + myName);
+
+// const a = 5;
+// const b = 2;
+// let myName = "mayu";
+
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log("hello " + myName);
+
+// myName = "kimmayu";
+
+// console.log("your new name is " + myName);
+
+// const amIFat = null;
+
+// let something;
+
+// console.log(something);
+
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// const thu = "thu";
+// const fri = "fri";
+// const sat = "sat";
+// const sun = "sun";
+
+//const dayOfWeek = mon + tue + wed + thu + fri + sat + sun;
+
+const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+// const nonsense = [1,  2, "hello" , false , null , true , undefined , "mayu"];
+
+console.log(dayOfWeek[4]);
+
+dayOfWeek.push("sun");
+
+console.log(dayOfWeek);
